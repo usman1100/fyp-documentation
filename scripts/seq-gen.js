03fc4d3bf2c914f8f1a81375787f4100cdd430f6
usman1100 <usmanahmed1100@gmail.com>
2022-01-24 02:36:47 +0500

    Activity Diagrams Changed seq script to generate act diagrams Made separate file for act diagrams Included acitivty.tex in design-index.tex

--- scripts/seq-gen.js
+++ scripts/seq-gen.js
@@ -2,13 +2,29 @@ function format(i){
     return i < 10 ? `0${i}` : i;
 }
 
+// sequence diagram generator
+
+// for (let i=1; i<20; i++){
+//     console.log(`
+//         \\begin{figure}[H]
+//             \\centering
+//             \\caption{Sequence Diagram ${i}}
+//             \\includegraphics[scale=0.5]{./diagrams/sequence/seq-${format(i)}.png}
+//             \\label{fig:seq-${format(i)}}
+            
+//         \\end{figure}
+//     `);
+// }
+
+
+// activity diagram generator
 for (let i=1; i<20; i++){
     console.log(`
         \\begin{figure}[H]
             \\centering
-            \\caption{Sequence Diagram ${i}}
-            \\includegraphics[scale=0.5]{./diagrams/sequence/seq-${format(i)}.png}
-            \\label{fig:seq-${format(i)}}
+            \\caption{Activity Diagram ${i}}
+            \\includegraphics[scale=0.5]{./diagrams/Activity Diagram/ad-${format(i)}.png}
+            \\label{fig:act-${format(i)}}
             
         \\end{figure}
     `);
