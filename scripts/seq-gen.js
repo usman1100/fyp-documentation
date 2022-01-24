e67647b5e89a14b7a03f5653d6e79e6885c048ca
usman1100 <usmanahmed1100@gmail.com>
2022-01-24 12:37:06 +0500

    Added Collaboration Diagrams All diagrams added

--- scripts/seq-gen.js
+++ scripts/seq-gen.js
@@ -1,20 +1,20 @@
-function format(i){
+function format(i) {
     return i < 10 ? `0${i}` : i;
 }
 
 // sequence diagram generator
 
-for (let i=1; i<20; i++){
-    console.log(`
-        \\begin{figure}[H]
-            \\centering
-            \\includegraphics[scale=0.5]{./diagrams/sequence/seq-${format(i)}.png}
-            \\caption{Sequence Diagram of UC-${i}}
-            \\label{fig:seq-${format(i)}}
+// for (let i = 1; i < 20; i++) {
+//     console.log(`
+//         \\begin{figure}[H]
+//             \\centering
+//             \\includegraphics[scale=0.5]{./diagrams/sequence/seq-${format(i)}.png}
+//             \\caption{Sequence Diagram of UC-${i}}
+//             \\label{fig:seq-${format(i)}}
             
-        \\end{figure}
-    `);
-}
+//         \\end{figure}
+//     `);
+// }
 
 
 // activity diagram generator
@@ -25,7 +25,21 @@ for (let i=1; i<20; i++){
 //             \\includegraphics[scale=0.5]{./diagrams/Activity Diagram/ad-${format(i)}.png}
 //             \\caption{Activity diagram of UC-${i}}
 //             \\label{fig:act-${format(i)}}
-            
+
 //         \\end{figure}
 //     `);
 // }
+
+
+// for collaboration diagram generator
+for (let i = 1; i <= 19; i++) {
+    console.log(`
+        \\begin{figure}[H]
+            \\centering
+            \\includegraphics[scale=0.5]{./diagrams/collaboration/cd-${i}.png}
+            \\caption{Collaboration diagram of UC-${i}}
+            \\label{fig:cd-${format(i)}}
+            
+        \\end{figure}
+    `);
+}
